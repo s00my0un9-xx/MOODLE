df8ad8841bd67d4c0f270be6481f8220ff3f8107
soomyoung <s00my0un9@gmail.com>
2026-03-13 17:11:14 +0900

    arrow icon 위치 조정 필요

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 $(function () {
-    // lnb menu list
-    $(".lnb__menu a").on("mouseenter focus", function () {
+    // lnb menu down
+    $(".gnb__browse").on('mouseenter', function () {
+        $(".lnb").stop().slideDown();
+    })
+
+    $(".lnb").on('mouseleave', function () {
+        $(".lnb").stop().slideUp();
+    })
+
+    $(".gnb__browse").on("mouseenter focus", function () {
+        $(".lnb").addClass("open");
+        $(this).addClass("on");
+    });
+    
+    $(".lnb").on("mouseleave", function () {
+        $(".lnb").removeClass("open");
+        $(".gnb__browse").removeClass("on");
+    });
+
+    // lnb menu content
+    $(".lnb__menu a").on('mouseenter focus', function () {
         const select = $(this).data("select");
         $(".recommend").removeClass("on");
         $("." + select).addClass("on");
@@ -8,4 +27,5 @@ $(function () {
         $(".lnb__menu a").removeClass("on");
         $(this).addClass("on");
     });
+
 });
